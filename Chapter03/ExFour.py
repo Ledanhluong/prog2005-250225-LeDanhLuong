399cfaa4422125737b7517088ae035eec8acfafa
Ledanhluong <116988270+Ledanhluong@users.noreply.github.com>
2026-03-04 11:22:01 +0700

    Create ExFour.py

---
+++ Chapter03/ExFour.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 5, 9, 8, 4]
+
+numbers.sort()
+print("After sort:", numbers)
+
+numbers.reverse()
+print("After reverse:", numbers)
+
+count_5 = numbers.count(5)
+print("Number of 5:", count_5)
